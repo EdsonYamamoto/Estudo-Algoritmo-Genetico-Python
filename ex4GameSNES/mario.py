f2b33027ebe583af2394c111323dd9c350bd4061
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2020-10-21 15:15:30 -0300

    jogo mario

--- ex4GameSNES/mario.py
+++ ex4GameSNES/mario.py
@@ -9,8 +9,8 @@ done = False
 while not done:
     env.render()
 
-    # action = env.action_space.sample()
-    action = [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0]
+    #action = env.action_space.sample()
+    action = [1, 0, 0, 0, 0, 0, 0, 1]
 
     ob, rew, done, info = env.step(action)
     print("Action ", action, "Reward ", rew)
